8d64c15bbf38a27cdfe8c2ad7dc6161064c89e35
Sagarsash <125851100+Sagarsash@users.noreply.github.com>
2024-01-02 16:36:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+  strings: ["Programmer", "Web Developer", "Cricket Player", "Chess Player", "Carrom Player"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
